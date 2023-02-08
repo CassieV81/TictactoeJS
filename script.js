4dc9b0ad93e879bdfdc71e6358b1bc31315bb34e
90500967 <90500967@FEC-8BH2TL3>
2023-02-08 15:28:50 +0100

    Add a random generation for computer play

--- script.js
+++ script.js
@@ -2,12 +2,25 @@
 
 const grids = document.querySelectorAll('.grid');
 
-const player = (name, marker) => {
-  return { name, marker };
+const gamePlay = () => {
+  const player1 = 'o';
+  const player2 = 'x';
+
+  const addMarker = () => {
+    grids.forEach(grid => {
+      grid.addEventListener('click', function() {
+        grid.innerText = player1;
+      });
+    });
+  }
+
+  const computerPlay = () => {
+    let random = Math.floor(Math.random() * 10);
+    grids[random].innerText = player2;
+  }
+
+  return addMarker(), computerPlay();
 }
 
-grids.forEach(grid => {
-  grid.addEventListener('click', function() {
-    grid.innerText = 'o';
-  });
-});
+const play = gamePlay();
+
