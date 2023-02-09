5e4e4caa39816d26f3d5446b92c99d94207d07bc
90500967 <90500967@FEC-8BH2TL3>
2023-02-09 14:55:31 +0100

    Add win display and functions

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 'use strict'
 
 const grids = document.querySelectorAll('.grid');
+const message = document.getElementById('message');
+const popUp = document.querySelector('.popUpWrap');
+const restart = document.querySelector('.restart');
+const close = document.querySelector('.close');
 let gameInProgress = false;
+let gameOver = false;
 
 const playGame = (player, marker) => {
 
 
   const humanPlay = () => {
+    if (gameOver) {
+      return;
+    }
     player = 'Player 1';
     marker = 'x';
     grids.forEach(grid => {
@@ -26,6 +34,9 @@ const playGame = (player, marker) => {
   };
   
   const computerPlay = (player, marker) => {
+    if (gameOver) {
+      return;
+    }
     player = 'Computer';
     marker = 'o';
     let random = grids[Math.floor(Math.random() * grids.length)];
@@ -37,24 +48,29 @@ const playGame = (player, marker) => {
       computerPlay();
     }
   };
+  const winMessage = () => {
+    message.innerText = player + ' wins';
+    popUp.style.display = 'block';
+    gameOver = true;
+  }
 
   const checkWin = (player, marker) => {
     if (grids[0].innerText == marker && grids[1].innerText == marker && grids[2].innerText == marker) {
-      console.log(`${player} wins!`);
+      winMessage();
     } else if (grids[3].innerText == marker && grids[4].innerText == marker && grids[5].innerText == marker) {
-      console.log(`${player} wins!`);
+      winMessage();
     } else if (grids[6].innerText == marker && grids[7].innerText == marker && grids[8].innerText == marker) {
-      console.log(`${player} wins!`);
+      winMessage();
     } else if (grids[0].innerText == marker && grids[3].innerText == marker && grids[6].innerText == marker) {
-      console.log(`${player} wins!`);
+      winMessage();
     } else if (grids[1].innerText == marker && grids[4].innerText == marker && grids[7].innerText == marker) {
-      console.log(`${player} wins!`);
+      winMessage();
     } else if (grids[2].innerText == marker && grids[5].innerText == marker && grids[8].innerText == marker) {
-      console.log(`${player} wins!`);
+      winMessage();
     } else if (grids[0].innerText == marker && grids[4].innerText == marker && grids[8].innerText == marker) {
-      console.log(`${player} wins!`);
+      winMessage();
     } else if (grids[2].innerText == marker && grids[4].innerText == marker && grids[6].innerText == marker) {
-      console.log(`${player} wins!`);
+      winMessage();
     }
   }
 
